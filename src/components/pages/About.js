0806a4de7f7f62b84ddf56d281ff0c957b3d8f2d
Chris M <chris@chrismabey.dev>
2023-07-26 17:04:18 -0300

    updated dev dependencies and navbar styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,8 @@ const About = () => {
     <Fragment>
       <h1>About This App</h1>
       <p>App to search Github users</p>
-      <p>Version: 1.0.0</p>
+      <p>Created using React</p>
+      <p>Version: 1.0.1</p>
     </Fragment>
   )
 }
